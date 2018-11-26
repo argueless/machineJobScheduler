c0df8c10aafb7502ac0c99ca4684c552c2c01057
argueless <dargue145@gmail.com>
2018-11-26 12:26:30 -0600

    output job schedule to a txt file

---
+++ machineJobScheduler/src/machineJobScheduler/Job.java
@@ -0,0 +1,42 @@
+package machineJobScheduler;
+
+public class Job {
+
+	int jobNumber;
+    int start;
+    int end;
+
+    public Job(int jobNumber, int start, int end) {
+    		this.jobNumber = jobNumber;
+        this.start = start;
+        this.end = end;
+    }
+
+    public int getJobNumber() {
+    		return jobNumber;
+    }
+    
+    public void setJobNumber(int jobNumber) {
+    		this.jobNumber = jobNumber;
+    }
+    
+    public int getStart() {
+        return start;
+    }
+
+    public void setStart(int start) {
+        this.start = start;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+
+    public void setEnd(int end) {
+        this.end = end;
+    }
+
+    public String toString() {
+        return this.jobNumber + " " + this.start + " " + this.end;
+    }
+}
--- machineJobScheduler/src/machineJobScheduler/Main.java
+++ machineJobScheduler/src/machineJobScheduler/Main.java
@@ -3,19 +3,31 @@ package machineJobScheduler;
 import java.io.File;
 import java.util.Scanner;
 import java.util.Stack;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 
 public class Main {
 	
+	static int machine=0;	// initialize variable for number of machines
+	
 	public static void main(String[] args) {
         try{
+        		// Read input file
             System.out.print("Enter file name:");
             Scanner input = new Scanner(System.in);
             File file = new File(input.nextLine());
             input = new Scanner(file);
             
-            Stack<Job> jb = new Stack<Job>();	// Stack to store jobs
+            // get number of machines from first line of input file
+            String first = input.nextLine();
+            String[] f = first.split(" ");
+            machine = Integer.parseInt(f[1]);
+            
+            // Stack to store jobs
+            Stack<Job> jb = new Stack<Job>();
             int i = 1;
-            while (input.hasNextLine()) {	// Read input into a Stack
+            // Read input into a Stack
+            while (input.hasNextLine()) {
                 String line = input.nextLine();
                 String[] l = line.split(" ");
                 int jobNumber = i;
@@ -27,14 +39,27 @@ public class Main {
                 i++;
             }
             input.close();
-            sortstack(jb);
-        }
-        catch (Exception ex) {
+            // Sort input by Job's finish time
+            // Create array for schedule by JobNumber
+            int[][] schedule = scheduleJobs(sortstack(jb), machine);	
+            
+            // Output Machine Job Schedule to a txt file
+    			PrintStream out = new PrintStream(new FileOutputStream("Output.txt"));
+    			for (int r=0; r<schedule.length; r++) {
+    				for (int c=0; c<schedule[r].length; c++) {
+    					out.print(schedule[r][c] + "\t");
+    				}
+    				out.println("\n");
+    			}
+    			out.close();
+    			
+        }catch (Exception ex) {
             ex.printStackTrace();
         }
-    }
+	}
+    
 	
-	// Sort Stack by job's end time
+	// Method to Sort Stack by job's end time
 	private static Stack<Job> sortstack(Stack<Job> input) {
 		Stack<Job> tmpStack = new Stack<Job>(); 
         while(!input.isEmpty()) 
@@ -47,8 +72,8 @@ public class Main {
             while(!tmpStack.isEmpty() && tmpStack.peek().end  
                                                  > tmp.end) 
             { 
-                // pop from temporary stack and  
-                // push it to the input stack 
+            // pop from temporary stack and  
+            // push it to the input stack 
             input.push(tmpStack.pop()); 
             } 
               
@@ -58,45 +83,52 @@ public class Main {
         return tmpStack;
 	}
 
-}
-class Job {
+	// Method to create array for Job schedule by Job Number (final output)
+	// jobs is a sorted stack
+	// m is number of machines
+	public static int[][] scheduleJobs(Stack<Job> jobs, int m) {
+		
+		int i, nj=0;	// nj is number of jobs
+		
+		Job[][] machines = new Job[m][]; // Array for machines that holds Jobs. Needed to compare jobs
+		int[][] schedule = new int[m][];	// Array showing schedule by Job Number. (final output)
+		
+		// first row is number of jobs
+		//machines[1][0] = jobs.pop(); // first job is always chosen
+		schedule[1][0] = machines[1][0].jobNumber;
+		
+		// Schedule jobs to machines
+		while (!jobs.isEmpty()) {
+			for (i=1; i<=m; i++) {
+				// Assign job to machine if machine has no jobs
+				if (machines[i].length == 0) {
+					machines[i][machines[i].length] = jobs.pop();
+					nj++;
+				}
+				// Compare job's start time to job's end time
+				if (jobs.peek().getStart() >= machines[i][machines[i].length - 1].getEnd()) {
+					machines[i][machines[i].length] = jobs.pop();
+					nj++;
+				}
+				// delete job if it will not fit
+				else
+					jobs.pop();
+			}		
+		}
+		
+		// First row of array is number of jobs
+		schedule[0][0] = nj;
+		// Create array for for the schedule by job number
+		for (int t=1; t<=machines.length; t++) {
+			for (int s=0; s < machines[t].length; s++) {
+				schedule[t][s] = machines[t][s].getJobNumber();
+			}
+		}
+		
+		return schedule;
+		
+	}
 	
-	int jobNumber;
-    int start;
-    int end;
-
-    public Job(int jobNumber, int start, int end) {
-    		this.jobNumber = jobNumber;
-        this.start = start;
-        this.end = end;
-    }
-
-    public int getJobNumber() {
-    		return jobNumber;
-    }
-    
-    public void setJobNumber(int jobNumber) {
-    		this.jobNumber = jobNumber;
-    }
-    
-    public int getStart() {
-        return start;
-    }
-
-    public void setStart(int start) {
-        this.start = start;
-    }
-
-    public int getEnd() {
-        return end;
-    }
-
-    public void setEnd(int end) {
-        this.end = end;
-    }
-
-    public String toString() {
-        return this.jobNumber + " " + this.start + " " + this.end;
-    }
 }
+
 
